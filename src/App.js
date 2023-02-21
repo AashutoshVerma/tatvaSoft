d4ab09d865f550bff4096c9e192a0cc17b8f2f20
AashutoshVerma <aashutoshverma007@gecg28.ac.in>
2023-02-21 21:39:47 +0530

    Day 1 task completed

--- src/App.js
+++ src/App.js
@@ -4,20 +4,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <strong>
+
+        <p>This is sample for text for understanding the rendering of html code in React.</p>
+        <p>Below given is the table of Student Details</p>
+
+        </strong>
+        <table border="1">
+            <tr>
+                <th>Name</th>
+                <th>Enrollment No.</th>
+            </tr>
+            <tr>
+                <td>Aashutosh Verma</td>
+                <td>200130116010</td>
+            </tr>
+            <tr>
+                <td>Uttam Makwana</td>
+                <td>200130116035</td>
+            </tr>
+            <tr>
+                <td>Divyesh Bhadrka</td>
+                <td>200130116041</td>
+            </tr>
+            <tr>
+                <td>Aaditya Chaturvedi</td>
+                <td>200130116059</td>
+            </tr>
+        </table>
+    </center>
     </div>
   );
 }
